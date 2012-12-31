cdbcc63c96348cea7771a9f0cfe8002da30dad66
guofei <guofei1986@gmail.com>
2013-01-01 03:21:40 +0900

    memecached set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,6 @@ Rabel::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.cache_store = :dalli_store, ENV["MEMCACHIER_SERVERS"].split(","),
+  config.cache_store = :dalli_store, ENV["MEMCACHIER_SERVERS"],
   {:username => ENV["MEMCACHIER_USERNAME"], :password => ENV["MEMCACHIER_PASSWORD"]}
 end
